da03b91c1bb5bc33a0c22929139b00fe8487a1f8
Akshay Gore <agore1@mail.csuchico.edu>
2017-02-08 13:48:11 -0800

    added pages details

--- Book Viewer/src/containers/book-detail.js
+++ Book Viewer/src/containers/book-detail.js
@@ -4,18 +4,19 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component{
     render(){
-        
+
         if(!this.props.book)
             {
                 return <div> Select a book to get started</div>;
             }
         return(
-        <div> 
-            <h3> Details For :  </h3>    
-           <div> Title:  { this.props.book.title} </div>  
+        <div>
+            <h3> Details For :  </h3>
+           <div> Title:  { this.props.book.title} </div>
+           <div> Pages:  { this.props.book.pages} </div>
         </div>
-            
-        ); 
+
+        );
     }
 }
 
@@ -30,4 +31,3 @@ function mapStateToProps(state){
 
 
 export default connect(mapStateToProps)(BookDetail);
-
--- Book Viewer/src/reducers/reducer_books.js
+++ Book Viewer/src/reducers/reducer_books.js
@@ -2,12 +2,12 @@
 
 export default function(){
     return [
-        
-        {title: 'JavaScript : A New Start'},
-        {title:' Harry Potter and Half Blood Prince'},
-        {title:'Kung fu panda'},
-        {title:'Jungle Book'},
-        {title:'Hatim tai'}
-        
+
+        {title: 'JavaScript : A New Start', pages :120},
+        {title:' Harry Potter and Half Blood Prince', pages :156},
+        {title:'Kung fu panda', pages :340},
+        {title:'Jungle Book', pages :23},
+        {title:'Hatim tai', pages :123}
+
     ];
 }
