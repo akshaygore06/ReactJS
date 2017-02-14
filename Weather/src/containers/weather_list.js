e30c5aaff3ac173e50946d3eb683cadceb1de764
Akshay Gore <agore1@mail.csuchico.edu>
2017-02-14 13:38:47 -0800

    added google maps

--- Weather/src/containers/weather_list.js
+++ Weather/src/containers/weather_list.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Sparklines, SparklinesLine } from 'react-sparklines';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
    renderWeahter(cityData){
@@ -9,10 +10,12 @@ class WeatherList extends Component {
       const temp = cityData.list.map(weather => weather.main.temp);
       const pressure = cityData.list.map(weather => weather.main.pressure);
       const humidity = cityData.list.map(weather => weather.main.humidity);
+      const {lon,lat} = cityData.city.coord;
+      //const lat = cityData.city.coord.lat;
 
       return(
          <tr key={ name } >
-            <td>{ name }</td>
+            <td>< GoogleMap lon={lon} lat={lat}/></td>
             <td><Chart data = { temp} color="red" units= 'K'/></td>
             <td><Chart data = { pressure } color="green" units= 'hPa'/></td>
             <td><Chart data = { humidity } color="orange" units= '%'/></td>
